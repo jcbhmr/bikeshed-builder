5bad13fecc3b50d70522c8068e9088c4651b0d83
Jacob Hummer <jcbhmr@outlook.com>
2024-05-05 18:26:03 -0500

    save

--- task.py
+++ task.py
@@ -4,7 +4,7 @@ import subprocess
 import shutil
 
 def build():
-    subprocess.run([shutil.which("pyinstaller"), "-y", "--collect-all", "bikeshed", "bikeshed.py"])
+    subprocess.run([shutil.which("pyinstaller"), "-y", "--collect-all", "bikeshed", "bin/bikeshed.py"])
 
 task_name = sys.argv[1] or 0/0
 task = { "build": build }[task_name] or 0/0
